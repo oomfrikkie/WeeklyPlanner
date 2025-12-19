b93595fa51b4f508ec8dd6be1ebf4ad9af914137
Derjen Frick <derjenyt@gmail.com>
2025-12-19 16:22:10 +0100

    added init.sql

---
+++ data_init/init.sql
@@ -0,0 +1,10 @@
+-- init.sql
+
+CREATE TABLE IF NOT EXISTS accounts (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    surname VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
